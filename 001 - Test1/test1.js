d54456be5643c13a88cdb2d570314cb4973948c9
danielhowells99 <danielhowells99@icloud.com>
2024-06-14 20:52:01 -0400

    update website

--- 001 - Test1/test1.js
+++ 001 - Test1/test1.js
@@ -8,6 +8,9 @@ function draw() {
   rotateX(accelerationX * 0.01);
   rotateY(accelerationY * 0.01);
   box(100, 100, 100);
+  fill(0)
+  noStroke()
+  text(accelerationX,0,0)
 }
 
 function mousePressed(){
