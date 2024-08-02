4b992d4ffccd38c1ca58a06c0ae17a1f90cb4339
danielhowells99 <danielhowells99@icloud.com>
2024-08-02 15:12:50 -0400

    update website

--- starfield/starfield.js
+++ starfield/starfield.js
@@ -26,9 +26,9 @@ function isTouchDevice() {
 }
 
 if (isTouchDevice()){
-	ontouchmove = function(e){mouse = {x: e.touches[0].clientX/canvas.width, y: 1-e.touches[0].clientY/canvas.height}}
-	ontouchstart = function(e){mouseForce = 1.0;}
-	ontouchend = function(e){mouseForce = 0.0;}
+	ontouchmove = function(e){mouse = {x: e.touches[0].clientX/canvas.width, y: 1-e.touches[0].clientY/canvas.height};mouseForce = 1.0;}
+	ontouchstart = function(e){mouse = {x: e.touches[0].clientX/canvas.width, y: 1-e.touches[0].clientY/canvas.height};mouseForce = 1.0;}
+	ontouchend = function(e){mouse = {x: e.touches[0].clientX/canvas.width, y: 1-e.touches[0].clientY/canvas.height};mouseForce = 0.0;}
 }
 onmousemove = function(e){mouse = {x: e.clientX/canvas.width, y: 1-e.clientY/canvas.height};mouseForce = 1.0;}
 
