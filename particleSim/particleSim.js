8f52819e5e50e80ba3b90c2b5743cd2d3a2a35a0
danielhowells99 <danielhowells99@icloud.com>
2024-11-06 19:47:43 -0500

    update website

--- particleSim/particleSim.js
+++ particleSim/particleSim.js
@@ -23,7 +23,7 @@ let partCol = [1-parts[1]/255,1-parts[2]/255,1-parts[3]/255]
 gl.clearColor(parts[1]/255, parts[2]/255, parts[3]/255, 0.0);
 
 let scale = 2;
-let screenScale = 1.5;
+let screenScale = 1.0;
 let screenBuffer = createScreenFramebuffer(gl,scale);
 
 function resizeCanvas() {
